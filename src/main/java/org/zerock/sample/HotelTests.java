caa6ade3cb92fa447eb1e7506f1c0c7ed4c34023
MooMoongg <alchemist72727@gmail.com>
2025-02-11 21:13:59 +0900

    feat: HotelTest

--- src/main/java/org/zerock/sample/HotelTests.java
+++ src/main/java/org/zerock/sample/HotelTests.java
@@ -12,20 +12,20 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration("file:file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class SampleTests {
+public class HotelTests {
 
 	@Setter(onMethod_ = { @Autowired })
-	private Restaurant restaurant;
-
+	private SampleHotel hotel;
+	
 	@Test
 	public void testExist() {
-		assertNotNull(restaurant);
-
-		log.info(restaurant);
-		log.info("--------------------------");
-		log.info(restaurant.getChef());
+		
+		assertNotNull(hotel);
+		
+		log.info(hotel);
+		log.info("------------------");
+		log.info(hotel.getChef());
 	}
-
 }
